0a3f166a97aa01c5a08336d36ca956e42fc59429
H. Demby <hdemby@unc.edu>
2012-04-16 13:20:16 -0400

    Improved version of 'playLotto.py':   - all engine parts except evaluator:     - 'evaluator' currently part of getwinners module     - test for lottovalue and good play/draw paring     - converts string to list     - generated a ticket based on the game being played     - checks each play against a draw for the game     - evaluates matches and powerball match     - generates an outcome report

--- playLotto.py
+++ playLotto.py
@@ -1,50 +1,189 @@
-import re,os,math
+import re,os,math,random
+
 
 MegaMillions=(5,56,1,46)
 CashFive=(5,39,0,0)
 
+DEBUG=0
 
-def doPicks(nums,selections):
+##--------------------------
+## formatting and test
+##--------------------------
+
+## check format of lottery string
+## valid strings: "9-14-33[-45-55][:35]"
+LottoVAl=lambda a:re.compile("^((\s)?[0-9]{1,2}(\s)?-){2,4}[0-9]{1,2}(\s)?([:\|-](\s)?[0-9]{1,2}(\s)?)?$").match(a)
+#>>> LottoVAl("9-14-33-45-55:35")
+#>>> LottoVAl("9-14-33-45-55-35")
+#>>> LottoVAl("9 14 33 45 55 35")
+#>>> LottoVAl("9,14,33,45,55,35")
+
+
+def play2lst(play):
+  "convert play string to number list"
+  try:
+    assert LottoVAl(play), "'%s' is not a valid lotto string"%play
+    balls=play.replace(":","-")
+    balls=balls.replace("|","-")
+    balls=balls.replace(" ","")
+    #balls,bonus=balls.rsplit("-",1)
+    balls=balls.split("-")
+    #balls+=bonus
+    return balls
+  except AssertionError:
+    print "ERROR!: '%s' is not a valid lotto string"%play
+    print "Format must be: '[n]n-[n]n-[n]n[-[n]n-[n]n][:[n]n]'"
+    return
+  except TypeError:
+    print "type of '%s': "%play,type(play)
+  return
+#>>> play="9-22-24-40-54 : 13"; play2lst(play)
+#>>> play="12-20-21-27-29:5"; play2lst(play)
+#>>> play="9-22-24-40-54-13"; play2lst(play)
+#>>> play="9-22-24-40-54|13"; play2lst(play)
+
+
+def lst2play(list):
+  "convert list of picks in a play string"
+  pb=[]
+  try:
+    if len(list)<3 or len(list)>6:
+      raise ValueError
+    elif len(list)==6:
+      list,pb=list[:5],list[5:]
+    playstr="-".join("%s"%s for s in list)
+    if pb:
+      playstr+=":%s"%pb[0]
+    return playstr
+  except ValueError:
+    print "The entered list cannot be converted to a lotery ticket: ",list
+#>>> list=[9,12,24,40,54,13]; lst2play(list)
+#>>> list=[9,12,24,40,54]; lst2play(list)
+#>>> list=[19,22,24,40,45,1]; lst2play(list)
+#>>> list=[19,22,24]; lst2play(list)
+#>>> list=[19,22,24,40,45,54,13]; lst2play(list)
+#>>> list=[19,22]; lst2play(list)
+
+def GoodPair(l1,l2):
+  if DEBUG: print len(l1),len(l2), type(l1),type(l2)
+  return len(l1)==len(l2) and type(l1)==type([]) and type(l2)==type([])
+#>>> l1=[9,14,33,45,55,35]; l2=[9,14,33,45,55,35]; GoodPair(l1,l2)
+#>>> l1=[9,14,33,45,55,35]; l2=[14,33,45,55,35]; GoodPair(l1,l2)
+    
+
+#=====================================================================================
+##--------------------------
+##  game logic
+##--------------------------
+
+def playChk(play,draw):
+  "check for draw matches in play"
+  ## calculates number of matches for each draw value, 
+  ## 'bonus ball' is last value if it is used 
+  play=play2lst(play)
+  draw=play2lst(draw)
+  try:
+    ## verify 'play' and 'draw' are equivalent in format
+    assert GoodPair(play,draw),"entries are not compatible: %s, %s"%(play, draw)
+    #play=play.split("-")
+    #draw=draw.split("-")
+    result=[]
+    for each in draw:
+      matches=0
+      for pick in play:
+        if pick==each:
+          matches=1
+      result.append(matches)
+    if draw[-1]==play[-1]:
+      result[-1]=1
+  except AssertionError:
+    print "Bad play!",play,draw
+    result=[]
+  return result
+#>>> play="9-14-33-45-55-35"; draw="9-14-33-45-55-35"; playChk(play,draw)
+#>>> play="9-14-33-45-55-35"; draw="9-14-33-45-55-5"; playChk(play,draw)
+#>>> play="9-14-33-45-55-35"; draw="19-24-33-5-15-3"; playChk(play,draw)
+#>>> play="9-14-33-45-55:35"; draw="19-24-33-5-15:3"; playChk(play,draw)
+#>>> play="3-5-9-45-51:5"; draw="19-24-33-5-15:35"; playChk(play,draw)
+#>>> play="3-5-9-45-51:5"; draw="19-24-33-5-15:5"; playChk(play,draw)
+
+
+def genPicks(nums,selections):
   "pick 'nums' numbers from range of selections"
   picks=[]
-  choices=range(1,selections+1)
-  for x in range(0,nums):
+  choices=range(1,int(selections)+1)
+  for x in range(0,int(nums)):
     pick=choices.pop(int(math.floor(random.random()*len(choices))))
     picks.append(pick)
   return picks,choices
-#>>> picks,choices=getPicks(nums,selections); print picks; print choices
-
-
+#>>> picks,choices=getPicks(5,56); print picks; print choices
 
 def getTicket(game=MegaMillions,plays=5):
   "simulate a Mega-Millions lottery session"
   ticket=""
-  #plays=raw_input("Number of Plays?: ")
-  num,sel,mul,vals=game
-  for n in range(0,plays):
-     picks="%s-%s-%s-%s-%s"%tuple(sorted(getPicks(num,sel)[0]))
-     if mul:
-       multp=": %s"%tuple(getPicks(mul,vals)[0])
+  ## num= numbers to choose, sel= range of choices; pb=play powerball; vals= 
+  num,sel,pp,vals=game
+  for n in range(0,int(plays)):
+     picks="%s-%s-%s-%s-%s"%tuple(sorted(genPicks(num,sel)[0]))
+     if pp:
+       pb=":%s"%tuple(genPicks(pp,vals)[0])
      else:
-       multp=""
-     ticket+="%s %s\n"%(picks,multp)
+       pb=""
+     ticket+="%s%s\n"%(picks,pb)
   return ticket
+#>>> from playLotto import *
+#>>> ticket=getTicket(); print ticket
+#>>> draw=getTicket(plays=1); print draw
 
-#>>> game=MegaMillions; print PlayLotto(game,5)
+#>>> ticket=getTicket(MegaMillions,3); print ticket
+#>>> draw=getTicket(plays=1); print draw
 
 
-def PlayLotto(ticket,plays=26):
-  "find a winning lottery numbers in a list of tickets"
-  ## this is a simulation of playing a 'ticket' for a number of games. It will evaluate the wins for the
-  ## numbers entered based on the ticket:game. Default plays is 26, the max number of of 'multi-plays' allowed.
-  ## The code also keeps track of the cost and winnings to evaluate ROI
+##--------------------------
+##  simulations
+##--------------------------
+
+def getWinners(ticket,draw):
+  "check ticket for wins"
+  ## get the raw win data from a ticket  
   ##
-  ## ticket={'game':gameObj,'picks':[picklist]}
-  ## gameObj:
-  ##  name:
-  ##  payrules:
-  ##  
-  return cost,winnings,winlst
+  ##
+  if DEBUG: print "**** Draw: %s ****"%draw
+  if DEBUG: print "draw",draw
+  #draw=play2lst(draw)
+  #if DEBUG: print "newdraw:",draw
+  cost=0
+  results={}
+  if DEBUG: print "========== results ====================="
+  for play in ticket.strip().split("\n"):
+    ## add cost of ticket to running total
+    if play:
+      cost+=1
+      ## check the ticket and tally wins:
+      if DEBUG: print "play:",play
+      #plaset=play2lst(play)
+      #if DEBUG: print "new play",plaset
+      result=playChk(play,draw)
+      if result:
+	mesg1=""
+        results.update({play:result})
+        if DEBUG: print "%16s\t%s"%(play,result),
+        degree=sum(result)
+        if degree>0:
+	  if degree>1:
+	    mesg1="Winner! match %s"%degree
+	  if result[-1]:
+	    mesg1+=" and ** Power Ball!! **"
+	if DEBUG: print mesg1    
+  if DEBUG: print "=======================================\n"    
+  return cost,results
+#>>> from playLotto import *
+#>>> ticket=getTicket(); print ticket
+#>>> draw=getTicket(plays=1); print draw
+#>>> cost,playresult=getWinners(ticket,draw); print rpt
+#>>> ticket=ticket.split("\n")
+#>>> for n in range(len(playresult.keys())): print ticket[n],playresult[ticket[n]]
+
 
 
 def myNumsUp(ticlst):
