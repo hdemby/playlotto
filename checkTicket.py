d749a4f875355e35933a4e04c1972233a2bc78bb
hdemby <hdemby@unc.edu>
2012-04-23 23:34:24 -0400

    Fixed default input for 'checkTicket.py'

--- checkTicket.py
+++ checkTicket.py
@@ -21,32 +21,43 @@ def main(ticket,draw,game=MegaMillions):
 
 if __name__=="__main__":
   "run application"
+  ## use command line parameters, take interactive input or use defaults
+  ## parm=True and sys.argv[n] or (True and raw_input("What ticket file?: ") or DEFAULT)
+  ##
   print "getting ticket..."
   try:
-    ticket="".join("%s"%s for s in open(sys.argv[1],'r').readlines()).strip()
+    ticket="".join("%s"%s for s in open(sys.argv[1],'r').readlines()).strip() # list to string w/o blank 
   except IndexError:
-    ticketfile=raw_input("What ticket file?: ")
+    ticketfile=True and raw_input("What ticket file?: ") or "mynumbers.lst"
     try:
       ticket="".join("%s"%s for s in open(ticketfile,'r').readlines()).strip()
     except IOError:
       print "That file dos not exist"
       raise ValueError
+  if DEBUG: print ticketfile
   try:
     draw=sys.argv[2]
   except IndexError:
     draw=raw_input("what are the winning numbers?: ")
+    if not draw:
+      raise ValueError,"must enter a drawing result"
+  if DEBUG: print draw
   try:
     game=eval(sys.argv[3])
   except IndexError:
-    game=eval(raw_input("What game?: "))
+    game=True and raw_input("What game?: ") or 'MegaMillions'
+    game=eval(game)
     #game=MegaMillions
+  if DEBUG: print game  
   try:
     price=int(sys.argv[4])
   except IndexError:  
-    price=int(raw_input("cost per play?: "))
-  except:
+    price=True and raw_input("cost per play?: ") or 1
+    price=int(price)
+  except ValueError:
     print "USAGE: ./checkTicket.py [<tktfile> <winning_numbers> <game> <price>]"
     sys.exit()
+  if DEBUG: print price
   print "Draw:  %s"%draw  
   winnings=main(ticket,draw,game)
   cost=len(ticket.split("\n"))*price
